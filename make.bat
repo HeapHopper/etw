82d564e0a333917b3d53dfcebb7c48f7134c348d
HeapHopper <tzvienir@gmail.com>
2026-04-21 15:01:06 +0300

    cmake - cleanup

--- make.bat
+++ make.bat
@@ -1,1 +1,11 @@
-cmake -S . -B build -G "Visual Studio 18 2026"
+@echo off
+
+:: Generate Solution (and Projects) file
+cmake -S . -B build
+
+:: opening .slnx (or .sln file) if Visual Studio is closed
+tasklist | find /I "devenv.exe" >nul && exit /b
+for %%f in (build\ETW.sln build\ETW.slnx) do if exist "%%f" (
+    start "" "%%f"
+    exit /b
+)
